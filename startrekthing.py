1703888e0cf3cb97febf6ce79d40d3806ead3881
dsetareh <dsetareh@vt.edu>
2022-01-02 05:42:43 -0500

    nearly works, indexing bug somewhere

--- startrekthing.py
+++ startrekthing.py
@@ -1,4 +1,4 @@
-import os, pymkv
+import os, pymkv, srt
 
 directory = os.getcwd()
 
@@ -19,10 +19,46 @@ def determine_track_number(filename):
     print('No english subtitles found, quitting')
     exit(1)
 
+
+def fix_subs(filename):
+    print('Fixing subs')
+    srt_file = open(filename[:-4] + '.srt', 'r')
+    raw_srt = srt_file.read()
+    srt_file.close()
+    srt_start = raw_srt.find('1') # idk
+    raw_srt = raw_srt[srt_start:]
+    sub_generator = srt.parse(raw_srt)
+    subtitles = list(sub_generator)
+    # group sub entries with identical timestamps
+    time_values = set(map(lambda x: x.start, subtitles)) # yoink
+    grouped_sub_ids = [[subtitle.index for subtitle in subtitles if subtitle.start == time] for time in time_values]
+    # combining sub entries could decrement the overall number of subs and mess up the indexing
+    # so just in case thisll sort the groups and work from the end
+    sorted_grouped_sub_ids = sorted(grouped_sub_ids, key=lambda d: d[0], reverse=True)
+    print('Found ' + str(len(sorted_grouped_sub_ids)) + ' groups to fix')
+    for group in sorted_grouped_sub_ids:
+        if len(group) == 1:
+            continue
+        fix_lines(subtitles, group)
+    # overwrite the file with the fixed subs
+    srt_file = open(filename[:-4] + '.srt', 'w')
+    srt_file.write(srt.compose(subtitles))
+    srt_file.close()
+    print('Subs Fixed!')
+
+    
+def fix_lines(subtitles, line_group):
+    for i in range(line_group[1], line_group[-1] + 1):
+        subtitles[line_group[0]].content = subtitles[line_group[0]].content + '<br />' + subtitles[i].content
+        subtitles[i].content = ''
+
+
+
 for entry in os.scandir(directory):
     if entry.path.endswith(".mkv") and entry.is_file():
         track_number = determine_track_number(entry.path)
         extract_subs(entry.path, track_number)
+        fix_subs(entry.path)
 
 
 
